a579f9fb48570d156609e35133d5d998bbf22e13
AamirAbbasi <aamirabbasi89@gmail.com>
2021-01-11 09:35:58 -0800

    Updated polytrode sorting script.

--- runPolytrodeSorting.m
+++ runPolytrodeSorting.m
@@ -1,22 +1,22 @@
-%% INVOKE CAMBRIDGE TECH POLYTRODE SPIKE SORTING USING SPYKING CIRCUS
+%% INVOKE CAMBRIDGE TECH POLYTRODE SPIKE SORTING ON SPYKING CIRCUS
 %  @author: Aamir Abbasi
-%  --------------------------------------------------
+%  ---------------------------------------------------------------------
 clc;clear;close all;tic;
 disp('running...');
 
 % Initialization (change rootpath and bmiBlocks according to your experiment identifiers)
-rootpath = 'Z:\TDTData\BMI_zBus_RS4-200629-101443\raw_data_RS4\';
-bmiBlocks = {'I064-200706_DAT_files','I064-200707_DAT_files','I064-200708_DAT_files'};
-paramsfilepath = 'Z:\TDTData\BMI_zBus_RS4-200629-101443\raw_data_RS4\params_tetrode.params';
+rootpath = 'Z:\M1_Cb_Reach\I076\RS4_Data\';
+bmiBlocks = {'I076-201211_DAT_files','I076-201212_DAT_files'};
+paramsfilepath = 'Z:\TDTData\BMI_zBus_RS4_RV2_Cb64-201130-100839\raw_data_RS4\params_polytrode.params';
 
 % Loop over blocks!
 for i=1:length(bmiBlocks)
   
   % Run sorting for polytrode 1
-  currentpath = [rootpath,bmiBlocks{i},'\Cb\Tetrode_0'];
+  currentpath = [rootpath,bmiBlocks{i},'\Cb\Polytrode_0'];
   cd (currentpath);
-  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_tet_0_0.params'))
-    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_tet_0_0.params'));
+  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_poly_0_0.params'))
+    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_poly_0_0.params'));
     if status == 1
       pause(2);
     else
@@ -24,13 +24,13 @@ for i=1:length(bmiBlocks)
       dbstop;
     end
   end
-  !activate circus & spyking-circus SU_CONT_Cb_tet_0_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
+  !activate circus & spyking-circus SU_CONT_Cb_poly_0_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
   
   % Run sorting for polytrode 2
-  currentpath = [rootpath,bmiBlocks{i},'\Cb\Tetrode_1'];
+  currentpath = [rootpath,bmiBlocks{i},'\Cb\Polytrode_1'];
   cd (currentpath);
-  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_tet_1_0.params'))
-    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_tet_1_0.params'));
+  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_poly_1_0.params'))
+    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_poly_1_0.params'));
     if status == 1
       pause(2);
     else
@@ -38,13 +38,13 @@ for i=1:length(bmiBlocks)
       dbstop;
     end
   end
-  !activate circus & spyking-circus SU_CONT_Cb_tet_1_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
+  !activate circus & spyking-circus SU_CONT_Cb_poly_1_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
   
   % Run sorting for polytrode 3
-  currentpath = [rootpath,bmiBlocks{i},'\Cb\Tetrode_2'];
+  currentpath = [rootpath,bmiBlocks{i},'\Cb\Polytrode_2'];
   cd (currentpath);
-  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_tet_2_0.params'))
-    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_tet_2_0.params'));
+  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_poly_2_0.params'))
+    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_poly_2_0.params'));
     if status == 1
       pause(2);
     else
@@ -52,13 +52,13 @@ for i=1:length(bmiBlocks)
       dbstop;
     end
   end
-  !activate circus & spyking-circus SU_CONT_Cb_tet_2_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
+  !activate circus & spyking-circus SU_CONT_Cb_poly_2_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
   
   % Run sorting for polytrode 4
-  currentpath = [rootpath,bmiBlocks{i},'\Cb\Tetrode_3'];
+  currentpath = [rootpath,bmiBlocks{i},'\Cb\Polytrode_3'];
   cd (currentpath);
-  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_tet_3_0.params'))
-    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_tet_3_0.params'));
+  if ~isfile(fullfile(currentpath,'SU_CONT_Cb_poly_3_0.params'))
+    status = copyfile(paramsfilepath,fullfile(currentpath,'SU_CONT_Cb_poly_3_0.params'));
     if status == 1
       pause(2);
     else
@@ -66,7 +66,7 @@ for i=1:length(bmiBlocks)
       dbstop;
     end
   end
-  !activate circus & spyking-circus SU_CONT_Cb_tet_3_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
+  !activate circus & spyking-circus SU_CONT_Cb_poly_3_0.dat -m filtering,whitening,clustering,fitting,merging,converting -c 6
   
 end
 runTime = toc;
